8ba2e4e88680741cfcdc42ed35204edd72622e63
minjeong9919 <minjeong9919@naver.com>
2025-01-06 15:06:45 +0900

    feat: scss 클래스 이름 자동완성 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import classNames from 'classnames/bind'
-import styles from './App.module.scss'
+import { useState } from 'react';
+import styles from './App.module.scss';
+import classNames from 'classnames/bind';
 
 function App() {
-  const cn = classNames.bind(styles)
+  const cn = classNames.bind(styles);
 
+  const [clicked, setClicked] = useState(false);
+  const handleClickButton = () => {
+    setClicked(!clicked);
+  };
   return (
     <div>
       프로젝트 세팅
-      <button className={cn('test-button')}>테스트 버튼</button>
+      <button
+        className={cn('testButton', { clicked: clicked })}
+        onClick={handleClickButton}>
+        테스트 버튼
+      </button>
+      <div>
+        프로젝트 세팅
+        {/* <button
+        className={classNames(styles.testButton, clicked && styles.clicked)}
+        onClick={handleClickButton}>
+        테스트 버튼
+      </button> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
