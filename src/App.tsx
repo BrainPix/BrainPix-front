36a7a898884f54c27419427de4405094ca7e3e55
minjeong9919 <minjeong9919@naver.com>
2025-01-06 13:45:03 +0900

    setting: scss & classnames 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css'
+import classNames from 'classnames/bind'
+import styles from './App.module.scss'
 
 function App() {
-  return <div>프로젝트 세팅</div>
+  const cn = classNames.bind(styles)
+
+  return (
+    <div>
+      프로젝트 세팅
+      <button className={cn('test-button')}>테스트 버튼</button>
+    </div>
+  )
 }
 
 export default App
