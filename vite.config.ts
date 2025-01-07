495a245a62a50bfdf6a7abae8a31831b6253e8be
minjeong9919 <minjeong9919@naver.com>
2025-01-08 01:59:34 +0900

    chore: svbr 라이브러리 설치 및 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    svgr({
+      // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+      include: '**/*.svg?react',
+    }),
+    react(),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
