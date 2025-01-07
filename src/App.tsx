6438e5f16aa736d4f2b2af5abfd55f157cbe3ed8
gaaahee <lgh4155@naver.com>
2025-01-07 23:24:17 +0900

    error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import classNames from 'classnames';
 import './styles/main.scss';
 
 function App() {
   const [isClicked, setIsClicked] = useState(false);
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
+
+  // useCallback 적용하여 ESLint 경고 방지
+  const handleClick = useCallback(() => {
+    setIsClicked((prev) => !prev);
+  }, []);
 
   return (
     <div className='container'>
@@ -16,26 +18,53 @@ function App() {
         <h3>카드 제목</h3>
         <p>카드 내용</p>
       </div>
-      <div className='cardDark'></div>
+      <div className='cardDark' />
 
       {/* 기본 버튼 */}
-      <button className='button'>기본 버튼</button>
+      <button 
+        className='button'
+      >
+        기본 버튼
+      </button>
 
       {/* 테두리 버튼 */}
-      <button className='button buttonOutlined'>테두리 버튼</button>
+      <button 
+        className='button buttonOutlined'
+      >
+        테두리 버튼
+      </button>
 
       {/* 블랙 버튼 */}
-      <button className='button buttonDark'>블랙 버튼</button>
+      <button 
+        className='button buttonDark'
+      >
+        블랙 버튼
+      </button>
 
       {/* 비활성화 버튼 */}
-      <button className='button' disabled>
+      <button 
+        className='button'
+        disabled
+      >
         비활성화 버튼
       </button>
 
       {/* 버튼 크기 테스트 */}
-      <button className='button buttonDark buttonSmall'>작은 버튼</button>
-      <button className='button buttonRounded buttonMedium'>중간 버튼</button>
-      <button className='button buttonOutlined buttonLarge'>큰 버튼</button>
+      <button 
+        className='button buttonDark buttonSmall'
+      >
+        작은 버튼
+      </button>
+      <button 
+        className='button buttonRounded buttonMedium'
+      >
+        중간 버튼
+      </button>
+      <button 
+        className='button buttonOutlined buttonLarge'
+      >
+        큰 버튼
+      </button>
 
       {/* 버튼 클릭 이벤트 테스트 */}
       <button
@@ -45,7 +74,9 @@ function App() {
         클릭 테스트 버튼
       </button>
 
-      <p>이 문장은 글로벌 SCSS 스타일이 적용되어야 합니다.</p>
+      <p>
+        이 문장은 글로벌 SCSS 스타일이 적용되어야 합니다.
+      </p>
     </div>
   );
 }
