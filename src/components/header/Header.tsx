d0430b5221f9036f11511142e85b727c067dd9a9
minjeong9919 <minjeong9919@naver.com>
2025-01-08 02:00:17 +0900

    feat: 헤더의 input 컴포넌트 구현

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,30 @@
 import classNames from 'classnames';
+import Search from '../../assets/icons/search.svg?react';
+
 import styles from './header.module.scss';
 
 export const Header = () => {
-  const MENU = {
+  const OPTION_MENU = {
     등록하기: '/register',
     요청하기: '/request',
     마이: 'my',
   };
   return (
     <div className={classNames(styles.container)}>
-      <input
-        className={classNames(styles.input)}
-        placeholder='어떤 아이디어를 찾으시나요? 키워드를 입력하세요'
-      />
+      <label className={classNames(styles.input)}>
+        <Search />
+        <input
+          className={classNames(styles.inputText)}
+          placeholder='어떤 아이디어를 찾으시나요? 키워드를 입력하세요'
+        />
+      </label>
       <menu>
-        {Object.entries(MENU).map(([menu, link]) => (
-          <a href={link}>{menu}</a>
+        {Object.entries(OPTION_MENU).map(([menu, link]) => (
+          <a
+            href={link}
+            key={menu}>
+            {menu}
+          </a>
         ))}
       </menu>
     </div>
