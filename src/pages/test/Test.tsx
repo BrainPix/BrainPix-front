fc650be319bb39f2fe7547c0a13cc4aac1c3679d
hyuke81 <rmsidthgusdl81@gmail.com>
2025-01-10 23:01:49 +0900

    test: 공통 버튼 컴포넌트 테스트

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -3,11 +3,24 @@ import { useState } from 'react';
 import classNames from 'classnames';
 import styles from './test.module.scss';
 
+//버튼그룹 test 
+import ButtonGroup from '../../components/button/ButtonGroup.tsx';
+
 export const Test = () => {
   const [clicked, setClicked] = useState(false);
   const handleClickButton = () => {
     setClicked(!clicked);
   };
+
+  //버튼그룹 test
+  const handleCancel = () => {
+    alert('취소 버튼 클릭됨');
+  };
+
+  const handleSubmit = () => {
+    alert('등록 버튼 클릭됨');
+  };
+
   return (
     <div className={classNames(styles.container)}>
       테스트 페이지입니다.
@@ -18,6 +31,11 @@ export const Test = () => {
         onClick={handleClickButton}>
         테스트 버튼
       </button>
+
+      {/* 버튼그룹 test */}
+      <div className={styles.buttonGroupWrapper}>
+        <ButtonGroup onCancel={handleCancel} onSubmit={handleSubmit} />
+      </div>
     </div>
   );
 };
