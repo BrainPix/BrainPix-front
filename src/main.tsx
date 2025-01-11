20930c3afaaf3723237dc6546c3773fffe40f06f
gaaahee <lgh4155@naver.com>
2025-01-11 20:54:01 +0900

    chore : scss 1.79 다운그레이드

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-<<<<<<< HEAD
-import './styles/main.scss';
-import './index.css';
-=======
 import './styles/global.scss';
->>>>>>> develop
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
