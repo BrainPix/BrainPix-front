91ec7d700832fb0bcb47ff8aef1b83822cb59211
hyuke81 <rmsidthgusdl81@gmail.com>
2025-01-12 22:36:03 +0900

    fix: svgr 플러그인 수정

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     svgr({
       // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
-      include: '**/*.svg',
+      include: '**/*.svg?react',
     }),
     react(),
   ],
