2b8ee068e7af44b928051bc9eb91799162c06a83
gaaahee <lgh4155@naver.com>
2025-01-15 19:48:51 +0900

    rename: components 파일에 sidebar 폴더 추가, 파일 분리

--- src/pages/my-page/MyPagePosts/MyPagePosts.tsx
+++ src/pages/my-page/MyPagePosts/MyPagePosts.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styles from './MyPagePosts.module.scss';
 import classNames from 'classnames';
 import { Header } from '../../../components/header/Header.tsx';
+import MyPageSidebar from '../../../components/sidebar/MyPageSidebar.tsx';
 
 const MyPagePosts = () => {
   const [activeTab, setActiveTab] = useState('아이디어 마켓');
@@ -20,56 +21,45 @@ const MyPagePosts = () => {
   return (
     <div className={styles.container}>
       <Header />
-      {/* 왼쪽 사이드바 */}
-      <aside className={styles.sidebar}>
-        <h2 className={styles.sidebarTitle}>마이페이지</h2>
-        <nav className={styles.nav}>
-          <div className={styles.navItem}>포트폴리오</div>
-          <div className={classNames(styles.navItem, styles.active)}>
-            게시물 관리
-          </div>
-          <div className={styles.navItem}>BrainPIX 지원 현황</div>
-          <div className={styles.navItem}>메신저</div>
-          <div className={styles.navItem}>저장</div>
-        </nav>
-      </aside>
+      <div className={styles.contentWrapper}>
+        <MyPageSidebar />
 
-      {/* 메인 콘텐츠 */}
-      <main className={styles.mainContent}>
-        <h1 className={styles.title}>
-          게시물 관리 <span className={styles.count}>11</span>
-        </h1>
+        <main className={styles.mainContent}>
+          <h1 className={styles.title}>
+            게시물 관리 <span className={styles.count}>11</span>
+          </h1>
 
-        {/* 탭 네비게이션 */}
-        <div className={styles.tabs}>
-          {['아이디어 마켓', '요청과제', '협업광장'].map((tab) => (
-            <div
-              key={tab}
-              className={classNames(styles.tabItem, {
-                [styles.activeTab]: activeTab === tab,
-              })}
-              onClick={() => setActiveTab(tab)}>
-              {tab}
-            </div>
-          ))}
-        </div>
-
-        {/* 게시물 리스트 */}
-        <div className={styles.postList}>
-          {posts.map((post) => (
-            <div
-              key={post.id}
-              className={styles.postCard}>
-              <div className={styles.thumbnail}></div>
-              <div className={styles.postInfo}>
-                <span className={styles.user}>{post.user}</span>
-                <p className={styles.title}>{post.title}</p>
-                <span className={styles.price}>{post.price}</span>
+          {/* 탭 네비게이션 */}
+          <div className={styles.tabs}>
+            {['아이디어 마켓', '요청과제', '협업광장'].map((tab) => (
+              <div
+                key={tab}
+                className={classNames(styles.tabItem, {
+                  [styles.activeTab]: activeTab === tab,
+                })}
+                onClick={() => setActiveTab(tab)}>
+                {tab}
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
+            ))}
+          </div>
+
+          {/* 게시물 리스트 */}
+          <div className={styles.postList}>
+            {posts.map((post) => (
+              <div
+                key={post.id}
+                className={styles.postCard}>
+                <div className={styles.thumbnail}></div>
+                <div className={styles.postInfo}>
+                  <span className={styles.user}>{post.user}</span>
+                  <p className={styles.title}>{post.title}</p>
+                  <span className={styles.price}>{post.price}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
