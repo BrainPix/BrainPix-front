e895c11676ea1b5ff9434c9cfabe49a2865a6f79
minjeong9919 <minjeong9919@naver.com>
2025-01-21 15:28:29 +0900

    feat: 유저 정보 컴포넌트 구현

---
+++ src/components/personal-profile/DescriptionTable.tsx
@@ -0,0 +1,70 @@
+import classNames from 'classnames';
+import { userProfileData } from '../../types/userData';
+import styles from './descriptionTable.module.scss';
+
+interface DescriptionTablePropsType {
+  userData: userProfileData;
+}
+
+export const DescriptionTable = ({ userData }: DescriptionTablePropsType) => {
+  const { phoneNumber, notion, github, skills, careers } = userData;
+  return (
+    <div className={classNames(styles.container)}>
+      <div className={classNames(styles.descriptionWrapper)}>
+        <h2 className={classNames(styles.title)}>자기소개</h2>
+        <p>자기소개 입니다. </p>
+      </div>
+      <div className={classNames(styles.descriptionWrapper)}>
+        <h2 className={classNames(styles.title)}>개별 정보</h2>
+        <table className={classNames(styles.infoTable)}>
+          <tbody>
+            <tr className={classNames(styles.row)}>
+              <th>연락처</th>
+              <td>{phoneNumber}</td>
+            </tr>
+            <tr className={classNames(styles.row)}>
+              <th>노션</th>
+              <td>{notion}</td>
+            </tr>
+            <tr className={classNames(styles.row)}>
+              <th>깃허브</th>
+              <td>{github}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={classNames(styles.descriptionWrapper)}>
+        <h2 className={classNames(styles.title)}>보유 기술</h2>
+        <div className={classNames(styles.skillsContainer)}>
+          {skills.map((skill) => (
+            <div
+              key={skill.name}
+              className={classNames(styles.skillWrapper)}>
+              <div className={classNames(styles.name)}>{skill.name}</div>
+              <hr className={classNames(styles.skillDivider)} />
+              <div className={classNames(styles.content)}>{skill.content}</div>
+              <hr className={classNames(styles.skillDivider)} />
+              <div className={classNames(styles.level)}>{skill.level}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={classNames(styles.descriptionWrapper)}>
+        <h2 className={classNames(styles.title)}>경력 사항</h2>
+        <div className={classNames(styles.careersContainer)}>
+          {careers.map((career) => (
+            <div
+              key={career.name}
+              className={classNames(styles.careerWrapper)}>
+              <div className={classNames(styles.name)}>{career.name}</div>
+              <hr className={classNames(styles.careerDivider)} />
+              <div className={classNames(styles.date)}>
+                {career.start} - {career.end}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/personalProfile/PersonalProfile.tsx
+++ src/pages/personalProfile/PersonalProfile.tsx
@@ -1,14 +1,48 @@
-import { useParams } from 'react-router-dom';
 import { ProfileCard } from '../../components/personal-profile/ProfileCard';
+import { DescriptionTable } from '../../components/personal-profile/DescriptionTable';
 import classNames from 'classnames';
 import styles from './personalProfile.module.scss';
+import { userProfileData } from '../../types/userData';
 
 export const PersonalProfile = () => {
-  const { id } = useParams();
-  console.log(id);
+  // const { id } = useParams();
+
+  const userData: userProfileData = {
+    id: 123,
+    name: 'MIN JEONG',
+    phoneNumber: '010-1234-5678',
+    notion: '노션 주소임',
+    github: '깃허브 주소임',
+    skills: [
+      {
+        name: '파이썬',
+        content: '파이썬을 이용한 게임 제작',
+        level: '상',
+      },
+      {
+        name: 'C언어',
+        content: 'C언어를 이용한 개발 경험',
+        level: '하',
+      },
+    ],
+    careers: [
+      {
+        name: 'SY TECH 인턴',
+        start: '2022/12',
+        end: '2023/06',
+      },
+      {
+        name: 'SY TECH 인턴',
+        start: '2024/12',
+        end: '2027/02',
+      },
+    ],
+  };
+
   return (
     <div className={classNames(styles.container)}>
       <ProfileCard />
+      <DescriptionTable userData={userData} />
     </div>
   );
 };
