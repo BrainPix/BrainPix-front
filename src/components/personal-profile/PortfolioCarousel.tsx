da63b1ee1def479688a5ff4e8c517f5bb410f53e
minjeong9919 <minjeong9919@naver.com>
2025-01-23 04:25:42 +0900

    feat: 포트폴리오 상세 팝업창 구현

--- src/components/personal-profile/PortfolioCarousel.tsx
+++ src/components/personal-profile/PortfolioCarousel.tsx
@@ -1,10 +1,27 @@
+import { useRef, useState } from 'react';
 import classNames from 'classnames';
-import styles from './portfolioCarousel.module.scss';
 import { Carousel } from '../common/Carousel/Carousel';
+import { PortfolioPopup } from './portfolioPopup';
+import { useOutsideClick } from '../../hooks/useOutsideClick';
+import styles from './portfolioCarousel.module.scss';
 
 export const PortfolioCarousel = () => {
+  const popupRef = useRef<HTMLDivElement>(null);
+  const [openPopup, setOpenPopup] = useState(false);
+  const handleClosePopup = () => {
+    setOpenPopup(false);
+  };
+
+  useOutsideClick({ ref: popupRef, handler: handleClosePopup });
+
   return (
     <div className={classNames(styles.container)}>
+      {openPopup && (
+        <PortfolioPopup
+          onClosePopup={handleClosePopup}
+          ref={popupRef}
+        />
+      )}
       <h1 className={classNames(styles.title)}>포트폴리오</h1>
       <Carousel
         gap={50}
@@ -15,7 +32,8 @@ export const PortfolioCarousel = () => {
         {new Array(10).fill(0).map((_, idx) => (
           <div
             key={idx}
-            className={classNames(styles.portfolio)}>
+            className={classNames(styles.portfolio)}
+            onClick={() => setOpenPopup(true)}>
             <div className={classNames(styles.image)} />
             <div className={classNames(styles.title)}>{idx}</div>
             <div className={classNames(styles.date)}>2024/12/25</div>
