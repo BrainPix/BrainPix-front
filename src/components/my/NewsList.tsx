2b7f3a92b9dcf977e2ba37a5949b4fc5da280dc8
minjeong9919 <minjeong9919@naver.com>
2025-01-24 02:37:58 +0900

    feat: 최근 소식 리스트 컴포넌트 구현

---
+++ src/components/my/NewsList.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import styles from './newsList.module.scss';
+import Arrow from '../../assets/icons/arrowRight.svg?react';
+
+interface NewsListPropsType {
+  isRead?: boolean;
+}
+
+export const NewsList = ({ isRead = false }: NewsListPropsType) => {
+  return (
+    <div className={classNames(styles.container, { [styles.isRead]: isRead })}>
+      {isRead && <div className={classNames(styles.tag)}>읽음</div>}
+      <div className={classNames(styles.pageRouteWrapper)}>
+        <span className={classNames(styles.page)}>아이디어 마켓</span>
+        <Arrow
+          width={24}
+          height={24}
+          className={classNames(styles.arrow)}
+        />
+        <span className={classNames(styles.page)}>담당자 Q&A</span>
+      </div>
+      <span className={classNames(styles.content)}>
+        Roria님이 댓글을 남겼습니다.
+      </span>
+      <button className={classNames(styles.tag)}>자세히</button>
+    </div>
+  );
+};
