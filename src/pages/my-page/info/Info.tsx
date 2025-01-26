4761e9aa4cb05fdce3a278246f38b9cd2a88aa77
minjeong9919 <minjeong9919@naver.com>
2025-01-27 01:06:23 +0900

    feat: 개별 정보 컴포넌트 구현

--- src/pages/my-page/info/Info.tsx
+++ src/pages/my-page/info/Info.tsx
@@ -14,6 +14,21 @@ export const Info = () => {
             자기 소개 내용임
           </div>
         </div>
+        <div>
+          <h1 className={classNames(styles.title)}>개별 정보</h1>
+          <div className={classNames(styles.individualInfoWrapper)}>
+            <div className={classNames(styles.labelWrapper)}>
+              <span>연락처</span>
+              <span>노션</span>
+              <span>깃허브</span>
+            </div>
+            <div className={classNames(styles.contentWrapper)}>
+              <span>01023451234</span>
+              <span>노션 주소</span>
+              <span>깃허브 주소</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
