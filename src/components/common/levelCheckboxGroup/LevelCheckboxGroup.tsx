da3e2043bc0d5309110105521f41b196110f9400
minjeong9919 <minjeong9919@naver.com>
2025-01-27 03:19:37 +0900

    feat: 상 중 하 선택 라디오 버튼 구현

---
+++ src/components/common/levelCheckboxGroup/LevelCheckboxGroup.tsx
@@ -0,0 +1,61 @@
+import { ChangeEvent, useState } from 'react';
+import classNames from 'classnames';
+import styles from './levelCheckboxGroup.module.scss';
+
+interface LevelCheckboxGroupPropsType {
+  initialValue?: string;
+  onChangeLevel: (value: string) => void;
+}
+
+export const LevelCheckboxGroup = ({
+  initialValue,
+  onChangeLevel,
+}: LevelCheckboxGroupPropsType) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleClickCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    onChangeLevel(e.target.value);
+  };
+
+  return (
+    <form className={classNames(styles.skillLevelWrapper)}>
+      <div>
+        <input
+          type='radio'
+          id='upper'
+          name='level'
+          value={'상'}
+          onChange={handleClickCheckbox}
+          defaultChecked={value === '상'}
+          className={classNames(styles.checkBox)}
+        />
+        <label htmlFor='upper'>상</label>
+      </div>
+      <div>
+        <input
+          type='radio'
+          id='middle'
+          name='level'
+          value={'중'}
+          onChange={handleClickCheckbox}
+          defaultChecked={value === '중'}
+          className={classNames(styles.checkBox)}
+        />
+        <label htmlFor='middle'>중</label>
+      </div>
+      <div>
+        <input
+          type='radio'
+          id='lower'
+          name='level'
+          value={'하'}
+          onChange={handleClickCheckbox}
+          defaultChecked={value === '하'}
+          className={classNames(styles.checkBox)}
+        />
+        <label htmlFor='lower'>하</label>
+      </div>
+    </form>
+  );
+};
--- src/pages/my-page/info/Info.tsx
+++ src/pages/my-page/info/Info.tsx
@@ -2,8 +2,12 @@ import classNames from 'classnames';
 import styles from './info.module.scss';
 
 import { MyProfileCard } from '../../../components/my/MyProfileCard';
+import { LevelCheckboxGroup } from '../../../components/common/levelCheckboxGroup/LevelCheckboxGroup';
 
 export const Info = () => {
+  const handleClick = (value: string) => {
+    console.log(value);
+  };
   return (
     <div>
       <MyProfileCard status='edit' />
@@ -26,6 +30,18 @@ export const Info = () => {
               <span>01023451234</span>
               <span>노션 주소</span>
               <span>깃허브 주소</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className={classNames(styles.title)}>보유 기술</h1>
+          <div className={classNames(styles.skillInfoWrapper)}>
+            <div className={classNames(styles.list)}>
+              <span className={classNames(styles.label)}>파이썬</span>
+              <span className={classNames(styles.content)}>
+                파이썬을 이용한 2D 게임 개발 경험
+              </span>
+              <LevelCheckboxGroup onChangeLevel={handleClick} />
             </div>
           </div>
         </div>
