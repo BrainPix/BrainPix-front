a6a31be5268b9792432df0180ecc4f063b131c26
minjeong9919 <minjeong9919@naver.com>
2025-01-27 00:55:57 +0900

    feat: 자기소개 카드 구현

--- src/pages/my-page/info/Info.tsx
+++ src/pages/my-page/info/Info.tsx
@@ -1,9 +1,20 @@
+import classNames from 'classnames';
+import styles from './info.module.scss';
+
 import { MyProfileCard } from '../../../components/my/MyProfileCard';
 
 export const Info = () => {
   return (
     <div>
       <MyProfileCard status='edit' />
+      <div className={classNames(styles.contentContainer)}>
+        <div>
+          <h1 className={classNames(styles.title)}>자기 소개</h1>
+          <div className={classNames(styles.introduceWrapper)}>
+            자기 소개 내용임
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
