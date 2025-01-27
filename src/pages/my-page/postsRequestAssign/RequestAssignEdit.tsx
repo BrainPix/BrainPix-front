478307ce36f3115142f76775004ea9b2406f8133
gaaahee <lgh4155@naver.com>
2025-01-27 17:59:13 +0900

    style: 마이페이지 요청과제 게시글 수정

--- src/pages/my-page/postsRequestAssign/RequestAssignEdit.tsx
+++ src/pages/my-page/postsRequestAssign/RequestAssignEdit.tsx
@@ -3,13 +3,19 @@ import styles from './requestAssignEdit.module.scss';
 export const RequestAssignEdit = () => {
   return (
     <div>
+      {/* 게시글 작성자의 이미지, 단체명, 수정&삭제 버튼 */}
       <div className={styles.header}>
-        <h1 className={styles.companyName}>SY TECH</h1>
+        <div className={styles.companyInfo}>
+          <div className={styles.companyImage}></div>
+          <div className={styles.companyName}>SY TECH</div>
+        </div>
         <div className={styles.actions}>
           <button className={styles.actionButton}>수정하기</button>
-          <button className={styles.actionButton}>삭제</button>
+          <div className={styles.buttonDivider}></div>
+          <button className={styles.actionButton}>삭제하기</button>
         </div>
       </div>
+      {/* 게시글 내용 */}
       <div>
         <div className={styles.imageWrapper}>
           <img
