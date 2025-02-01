d4dea1f8071b9ec4c7e1bbbedcf05e99e26e9de1
minjeong9919 <minjeong9919@naver.com>
2025-02-01 20:44:30 +0900

    refactor: 시작 페이지 css 수정된 디자인에 맞춰 수정

--- src/pages/sign-up/Signup.tsx
+++ src/pages/sign-up/Signup.tsx
@@ -12,23 +12,26 @@ export const Signup = () => {
     <div className={classNames(styles.container)}>
       <div className={classNames(styles.logo)}>로고</div>
       <h1 className={classNames(styles.headline)}>
-        회원가입하고
+        로그인하고
         <br /> 아이디어를 공유해보세요!
       </h1>
       <div className={classNames(styles.buttonWrapper)}>
         <button
           onClick={() => navigate('individual')}
-          className={classNames(styles.memberButton, styles.individual)}>
-          <Individual />
-          <span>개인회원</span>
+          className={classNames(styles.memberButton, 'buttonFilled-grey800')}>
+          <Individual stroke='#fafafa' />
+          <span>개인회원 로그인하기</span>
         </button>
         <button
           onClick={() => navigate('corporate')}
-          className={classNames(styles.memberButton)}>
+          className={classNames(styles.memberButton, 'buttonOutlined-grey500')}>
           <Corporate />
-          <span>기업회원</span>
+          <span>기업회원 로그인하기</span>
         </button>
       </div>
+      <p className={classNames(styles.signUpText)}>
+        아직 회원이 아니라면? <strong>회원가입하기</strong>
+      </p>
     </div>
   );
 };
