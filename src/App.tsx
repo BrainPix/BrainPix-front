4a5aac7cbf1b4a17ae2bc4bca113bca145ee6e6c
minjeong9919 <minjeong9919@naver.com>
2025-02-03 21:41:35 +0900

    feat: react query provider 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-  return <RouterProvider router={routes} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes}></RouterProvider>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
