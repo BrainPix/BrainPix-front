2c92c1aa292c1e22f90142633ff43543965337fe
gaaahee <lgh4155@naver.com>
2025-02-04 16:18:52 +0900

    routes.tsx 경로 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -20,6 +20,7 @@ import { PostsCollaboration } from './pages/my-page/postsCollaboration/PostsColl
 import { IdeaMarketEdit } from './pages/my-page/postsIdeaMarket/IdeaMarketEdit';
 import { RequestAssignEdit } from './pages/my-page/postsRequestAssign/RequestAssignEdit';
 import { CollaborationEdit } from './pages/my-page/postsCollaboration/CollaborationEdit';
+import { RequestAssignRegister } from './pages/my-page/postsRequestAssign/RequestAssignRegister';
 import { Portfolio } from './pages/my-page/portfolio/Portfolio';
 import { Message } from './pages/my-page/message/Message';
 import { Main } from './pages/main/main';
@@ -121,6 +122,10 @@ export const routes = createBrowserRouter([
           {
             path: '/my/posts/request-assign/edit/:postId',
             element: <RequestAssignEdit />,
+          },
+          {
+            path: '/my/posts/request-assign/register/:postId',
+            element: <RequestAssignRegister />,
           },
           {
             path: '/my/posts/collaboration/edit/:postId',
