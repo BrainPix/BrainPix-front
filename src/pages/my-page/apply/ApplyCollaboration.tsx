61d9f6e9ba743e75aeb0d279bbe1aa0cc36cd8b9
gaaahee <lgh4155@naver.com>
2025-02-05 20:38:11 +0900

    style: 마이페이지 지원 현황 - 협업광장 스타일 완성

--- src/pages/my-page/apply/ApplyCollaboration.tsx
+++ src/pages/my-page/apply/ApplyCollaboration.tsx
@@ -2,10 +2,11 @@ import styles from './applyCollaboration.module.scss';
 import { CardHeader } from '../../../components/my-page/apply/CardHeader';
 import { PostAuthorInfo } from '../../../components/my-page/apply/PostAuthorInfo';
 import { ApplyDetailsInfo } from '../../../components/my-page/apply/ApplyDetailsInfo';
+import { ApplyTable } from '../../../components/my-page/apply/ApplyTable';
 
 export const ApplyCollaboration = () => {
   const FORM_DATA = {
-    cardTitle: '요청 과제 지원 상세',
+    cardTitle: '협업 광장 지원 상세',
   };
   const APPLY_DATA = [
     {
@@ -14,9 +15,9 @@ export const ApplyCollaboration = () => {
       statusType: 'accept',
       date: '2024/12/24',
       seller: 'SEO YEON',
-      tab: '요청 과제',
+      tab: '협업 광장',
       category: '디자인',
-      itemName: '앱 개발 해주실 분',
+      itemName: '앱 개발 팀원 모집',
       part: '디자이너',
     },
     {
@@ -25,10 +26,24 @@ export const ApplyCollaboration = () => {
       statusType: 'reject',
       date: '2024/12/24',
       seller: 'SEO YEON',
-      tab: '요청 과제',
+      tab: '협업 광장',
       category: '디자인',
-      itemName: '앱 개발 해주실 분',
+      itemName: '앱 개발 팀원 모집',
       part: '디자이너',
+    },
+  ];
+  const TABLE_DATA = [
+    {
+      id: 'qweqr',
+      role: '디자이너',
+      current: 1,
+      total: 2,
+    },
+    {
+      id: 'qweqr',
+      role: '디자이너',
+      current: 1,
+      total: 2,
     },
   ];
 
@@ -36,7 +51,7 @@ export const ApplyCollaboration = () => {
   const rejectedApplications = APPLY_DATA.filter(apply => apply.statusType === 'reject');
 
   return (
-<div className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.sectionTitle}>
         <span className={styles.sectionMainTitle}>지원 현황</span>
         <span className={styles.sectionSubTitle}>지원 내역</span>
@@ -61,6 +76,20 @@ export const ApplyCollaboration = () => {
             itemName={apply.itemName}
             part={apply.part}
           />
+          <div className={styles.tableContainer}>
+            <div className={styles.tableHeader}>
+              <span>아이디</span>
+              <span className={styles.divider} />
+              <span>역할</span>
+              <span className={styles.divider} />
+              <span>인원수</span>
+              <span className={styles.divider} />
+              <span></span>
+            </div>
+            {TABLE_DATA.map((data) => (
+              <ApplyTable id={data.id} role={data.role} current={data.current} total={data.total} />
+            ))}
+          </div>
         </div>
       ))}
 
