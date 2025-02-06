e2c717f7fd1c8f17f436c3fcb9b0be34059fcbc5
gaaahee <lgh4155@naver.com>
2025-02-06 15:33:28 +0900

    style: 마이페이지 게시물 관리 - 아이디어 마켓 css 수정 완료

--- src/pages/my-page/postsIdeaMarket/PostsIdeaMarket.tsx
+++ src/pages/my-page/postsIdeaMarket/PostsIdeaMarket.tsx
@@ -1,25 +1,26 @@
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import styles from './postsIdeaMarket.module.scss';
 import { PurchaseStatus } from '../../../components/my-page/PurchaseStatus';
 import { IdeaMarketPostHeader } from '../../../components/my-page/IdeaMarketPostHeader';
 
 export const PostsIdeaMarket = () => {
+  const FORM_DATA = {
+    tab: '아이디어 마켓',
+    category: '디자인',
+  };
   const POST_DATA = [
     {
       id: 1,
       user: 'SEO YEON',
       title: '디자인 해드립니다',
       postImage: '',
-      price: 200000000,
+      price: 200000,
       ideaMarketAuth: 'ALL',
     },
   ];
 
   // 게시물 id 받아오기
   const { postId } = useParams<{ postId: string }>();
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const category = queryParams.get('category') || '카테고리 없음';
 
   // POST_DATA에서 postId에 맞는 데이터 찾기
   const post = POST_DATA.find((post) => post.id === Number(postId));
@@ -31,11 +32,17 @@ export const PostsIdeaMarket = () => {
   return (
     <div className={styles.postcardWrapper}>
       <IdeaMarketPostHeader
-        category={category}
+        tab={FORM_DATA.tab}
+        category={FORM_DATA.category}
         title={post.title}
         price={post.price}
         postId={post.id}
       />
+
+      <div className={styles.tableContainer}>
+        <div className={styles.tableTitle}>구매 현황</div>
+        <div className={styles.titleDivider}/>
+      </div>
       <div className={styles.purchaseCardWrapper}>
         <PurchaseStatus />
       </div>
