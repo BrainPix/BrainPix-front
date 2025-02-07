9a0f88b6898357955bf1c35df06a086a29592a8d
hyuke81 <rmsidthgusdl81@gmail.com>
2025-02-07 18:00:38 +0900

    refactor: 액세스 토큰 가능하도록

--- src/apis/detailPageAPI.ts
+++ src/apis/detailPageAPI.ts
@@ -1,21 +1,35 @@
 import axios from 'axios';
-import { IdeaMarketDetail } from '../types/detailPageType';
-import { RequsetDetail } from '../types/detailPageType';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const getIdeaMarketDetail = async (
-  ideaId: number,
-): Promise<IdeaMarketDetail> => {
+export const getIdeaMarketDetail = async (ideaId: number) => {
+  const token = localStorage.getItem('accessToken');
   const url = `${BASE_URL}/idea-markets/${ideaId}`;
-  const response = await axios.get<{ data: IdeaMarketDetail }>(url);
-  return response.data.data;
+  //const response = await axios.get<{ data: IdeaMarketDetail }>(url);
+
+  if (token) {
+    const { data } = await axios(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.data;
+  }
 };
 
-export const getRequestDetail = async (
-  ideaId: number,
-): Promise<RequsetDetail> => {
-  const url = `${BASE_URL}/idea-markets/${ideaId}`;
-  const response = await axios.get<{ data: RequsetDetail }>(url);
-  return response.data.data;
+export const getRequestDetail = async (taskId: number) => {
+  const token = localStorage.getItem('accessToken');
+  const url = `${BASE_URL}/request-tasks/${taskId}`;
+  //const response = await axios.get<{ data: RequsetDetail }>(url);
+
+  if (token) {
+    const { data } = await axios(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.data;
+  }
 };
