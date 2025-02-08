20f4295f4b15a51796083f7d2c0a457141a40218
gaaahee <lgh4155@naver.com>
2025-02-08 17:43:14 +0900

    refactor: purchaseAPI 요청 성공

--- src/apis/purchaseAPI.ts
+++ src/apis/purchaseAPI.ts
@@ -3,17 +3,25 @@ import { Purchase } from '../types/purchaseType';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+console.log('API BASE_URL: ', BASE_URL);
+
 export const getPurchases = async (
   page = 0,
   size = 10,
 ): Promise<Purchase[]> => {
   const url = `${BASE_URL}/supports/idea-market/purchases`;
 
-  const token = localStorage.getItem('token');
+  const API_TOKEN = localStorage.getItem('accessToken');
+
+  if (!API_TOKEN) {
+    console.error('API_TOKEN이 없습니다! 다시 로그인하세요.');
+    window.location.href = 'users/login';
+    return [];
+  }
 
   const response = await axios.get(url, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${API_TOKEN}`,
     },
     params: { page, size },
   });
