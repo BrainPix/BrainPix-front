810a1a49bad72940b8bee916d8f0b8c68775437d
hyuke81 <rmsidthgusdl81@gmail.com>
2025-02-09 23:12:53 +0900

    feat: 결제 데이터 타입 작성

---
+++ src/types/purchaseType.ts
@@ -0,0 +1,10 @@
+export interface IdeaMarketPurchase {
+  ideaId: number;
+  thumbnailImageUrl: string;
+  price: number;
+  title: string;
+  remainingQuantity: number;
+  sellerId: number;
+  name: string;
+  profileImageUrl: string;
+}
