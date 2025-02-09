1e270685211b05957910615a745772c02a54fe0c
hyuke81 <rmsidthgusdl81@gmail.com>
2025-02-09 22:08:40 +0900

    feat: CollaborationDetail 데이터 타입 추가

--- src/types/detailPageType.ts
+++ src/types/detailPageType.ts
@@ -59,3 +59,45 @@ export interface RequsetDetail {
   }[];
   openMyProfile: boolean;
 }
+
+export interface CollaborationDetail {
+  collaborationId: number;
+  title: string;
+  content: string;
+  category: string;
+  taskType: string;
+  deadline: string;
+  auth: string;
+  viewCount: number;
+  saveCount: number;
+  createdDate: string;
+  attachments: string[];
+  price: number;
+  paymentDuration: string;
+  link: string;
+  thumbnailImageUrl: string;
+  writer: {
+    writerId: number;
+    name: string;
+    profileImageUrl: string;
+    role: string;
+    specialization: string;
+    totalIdeas: number;
+    totalCollaborations: number;
+  };
+  recruitments: {
+    recruitmentId: number;
+    domain: string;
+    occupiedQuantity: number;
+    totalQuantity: number;
+    price: number;
+    paymentDuration: string;
+  }[];
+  openMembers: {
+    userId: number;
+    name: string;
+    domain: string;
+    openMyProfile: boolean;
+  }[];
+  openMyProfile: boolean;
+}
