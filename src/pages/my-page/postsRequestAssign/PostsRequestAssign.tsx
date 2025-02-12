fe605acc5f04d8a08ad9fd88ec2037bfa20118fd
gaaahee <lgh4155@naver.com>
2025-02-12 18:30:05 +0900

    feat: 마이페이지 게시물 관리 - 요청 과제 상세 조회 중 현재 인원 데이터 api 요청 성공

--- src/pages/my-page/postsRequestAssign/PostsRequestAssign.tsx
+++ src/pages/my-page/postsRequestAssign/PostsRequestAssign.tsx
@@ -44,7 +44,7 @@ export const PostsRequestAssign = () => {
         postId={post.ideaId}
       />
       <ApplyStatus applicationStatus={post.applicationStatus} />
-      <CurrentPeople />
+      <CurrentPeople currentMembers={post.currentMembers} />
     </>
   );
 };
