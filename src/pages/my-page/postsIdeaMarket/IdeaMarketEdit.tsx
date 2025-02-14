6e2aaed5770bb080cec0261b9f05a0c70ea148c1
gaaahee <lgh4155@naver.com>
2025-02-14 19:09:54 +0900

    feat: 마이페이지 아이디어 마켓 등록된 페이지 api 연동 중..

--- src/pages/my-page/postsIdeaMarket/IdeaMarketEdit.tsx
+++ src/pages/my-page/postsIdeaMarket/IdeaMarketEdit.tsx
@@ -1,8 +1,12 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getIdeaMarketDetail } from '../../../apis/detailPageAPI';
+import { IdeaMarketDetail } from '../../../types/detailPageType';
 import { PostFormAuthor } from '../../../components/my-page/PostFormAuthor';
 import { PostFormIdeaTitle } from '../../../components/my-page/PostFormIdeaTitle';
 import { PostFormContent } from '../../../components/my-page/PostFormContent';
 import QnASection from '../../../components/postdetail/QnASection';
-//import AuthorInfo from '../../../components/postdetail/AuthorInfo';
+import AuthorInfo from '../../../components/postdetail/AuthorInfo';
 
 export const IdeaMarketEdit = () => {
   const FORM_DATA = {
@@ -11,17 +15,33 @@ export const IdeaMarketEdit = () => {
     attachmentTitle: '첨부파일',
     attachmentFileName: '첨부파일 제목 입니다... .pdf',
   };
-  const USER_DATA = {
-    postId: 2,
-    userName: 'SEO YEON',
-    profileImage: null,
-  };
+  // const post = {
+  //   postId: 2,
+  //   userName: 'SEO YEON',
+  //   profileImage: null,
+  // };
+
+  const { ideaId } = useParams<{ ideaId: string }>();
+
+  const {
+    data: post,
+    isLoading,
+    isError,
+  } = useQuery<IdeaMarketDetail, Error>({
+    queryKey: ['ideaMarketDetail', ideaId],
+    queryFn: () => getIdeaMarketDetail(Number(ideaId)),
+    enabled: !!ideaId,
+  });
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (isError || !post) return <div>게시글을 찾을 수 없습니다.</div>;
+
   return (
     <div>
       {/* 게시물 작성자, 게시물 정보 */}
       <PostFormAuthor
-        userName={USER_DATA.userName}
-        profileImage={USER_DATA.profileImage}
+        userName={post?.writer?.name || ''}
+        profileImage={post?.writer?.profileImageUrl || ''}
       />
       {/* 게시물 제목, 게시물 정보 */}
       <PostFormIdeaTitle />
@@ -36,7 +56,14 @@ export const IdeaMarketEdit = () => {
       <QnASection />
 
       {/* 작성자 정보 */}
-      {/*<AuthorInfo />*/}
+      <AuthorInfo
+        name={post?.writer?.name || ''}
+        profileImageUrl={post?.writer?.profileImageUrl || ''}
+        role={post?.writer?.role || ''}
+        specialization={post?.writer?.specialization || ''}
+        totalIdeas={post?.writer?.totalIdeas || 0}
+        totalCollaborations={post?.writer?.totalCollaborations || 0}
+      />
     </div>
   );
 };
