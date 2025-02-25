b3159c808837397933687595e6cf0f56e4059dec
minjeong9919 <minjeong9919@naver.com>
2025-02-25 13:19:25 +0900

    chore: 코드 최적화를 위한 rollup-visualizer 라이브러리 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      treeshake: true,
+    },
+    cssCodeSplit: true,
+  },
   plugins: [
     svgr({
       // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
       include: '**/*.svg?react',
     }),
     react(),
+    [visualizer() as PluginOption],
   ],
   css: {
     preprocessorOptions: {
